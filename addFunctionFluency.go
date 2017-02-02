3dd184b60a146b7a9d1719454c02bbfe6a6a0bf2
David Green <dave@activelylazy.co.uk>
2017-02-02 21:26:29 +0000

    Output actual params & return types

--- addFunctionFluency.go
+++ addFunctionFluency.go
@@ -17,22 +17,21 @@ type FluentAddFunctionParameter struct {
 	washer       *Washer
 	file         *WashFile
 	functionName string
-	params       map[string]string
+	params       []field
 }
 
 // FluentAddFunctionWithReturn is a fluent structure for defining return types of a new function
 type FluentAddFunctionWithReturn struct {
-	washer          *Washer
-	file            *WashFile
-	functionName    string
-	params          map[string]string
-	returnTypeNames []string
+	washer       *Washer
+	file         *WashFile
+	functionName string
+	params       []field
+	returnFields []field
 }
 
 // WithParameter adds a parameter to the function
 func (e FluentAddFunction) WithParameter(name string, typeName string) FluentAddFunctionParameter {
-	params := make(map[string]string)
-	params[name] = typeName
+	params := []field{newField(name, typeName)}
 	return FluentAddFunctionParameter{
 		washer:       e.washer,
 		file:         e.file,
@@ -43,41 +42,24 @@ func (e FluentAddFunction) WithParameter(name string, typeName string) FluentAdd
 
 // Returning declares the return type(s) of the new function
 func (e FluentAddFunctionParameter) Returning(typeNames ...string) FluentAddFunctionWithReturn {
-	return FluentAddFunctionWithReturn{
-		washer:          e.washer,
-		file:            e.file,
-		functionName:    e.functionName,
-		params:          e.params,
-		returnTypeNames: typeNames,
+	returnFields := make([]field, len(typeNames))
+	for i := 0; i < len(typeNames); i++ {
+		returnFields[i] = newField("", typeNames[i])
 	}
-}
-
-func (e FluentAddFunctionWithReturn) getParamFields() []field {
-	return []field{
-		field{
-			fieldName: "s",
-			typeName:  "string",
-		}}
-}
-
-func (e FluentAddFunctionWithReturn) getResultFields() []field {
-	return []field{
-		field{
-			fieldName: "",
-			typeName:  "int",
-		},
-		field{
-			fieldName: "",
-			typeName:  "bool",
-		},
+	return FluentAddFunctionWithReturn{
+		washer:       e.washer,
+		file:         e.file,
+		functionName: e.functionName,
+		params:       e.params,
+		returnFields: returnFields,
 	}
 }
 
 // Build creates the new function and writes the updated file to disk
 func (e FluentAddFunctionWithReturn) Build() {
 	log.Printf("Adding function %s to %s", e.functionName, e.file.targetFilename)
-	params := e.getParamFields()
-	results := e.getResultFields()
+	params := e.params
+	results := e.returnFields
 	addFunction(e.file.file, e.functionName, params, results,
 		[]ast.Stmt{
 		// &ast.ReturnStmt{
@@ -88,4 +70,11 @@ func (e FluentAddFunctionWithReturn) Build() {
 		// },
 		})
 	e.file.write()
+}
+
+func newField(name string, typeName string) field {
+	return field{
+		fieldName: name,
+		typeName:  typeName,
+	}
 }
