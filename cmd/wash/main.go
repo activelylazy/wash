26f04906e3a2eae11d2d883ded52e2a19875c1b7
David Green <dave@activelylazy.co.uk>
2017-02-09 22:17:15 +0000

    Remove dead code

--- cmd/wash/main.go
+++ cmd/wash/main.go
@@ -7,12 +7,6 @@ import (
 )
 
 func main() {
-	// fset := token.NewFileSet()
-	// path := "C:\\Users\\Dave\\Documents\\Projects\\Go\\src\\github.com\\activelylazy\\generated-vending"
-	// _, err := parser.ParseDir(fset, path, nil, parser.AllErrors)
-	// if err != nil {
-	// 	log.Fatalf("Error parsing: %v", err)
-	// }
 
 	washer, err := wash.NewWasher("C:\\Users\\Dave\\Documents\\Projects\\Go\\src\\github.com\\activelylazy\\generated-vending")
 	if err != nil {
@@ -32,37 +26,4 @@ func main() {
 		[]string{"0", "false"}).
 		Add(washer)
 
-	//.
-	// WhichWhenGiven(invalidCoin).
-	// Returns(0, false)
-
-	// packageName := "vending"
-	// // fileName := "vending.go"
-	// f := newFile(packageName)
-	// addImport(f, "", "\"github.com/moo\"")
-	// addFunction(f, "validateCoin", []field{
-	// 	field{
-	// 		fieldName: "s",
-	// 		typeName:  "string",
-	// 	}},
-	// 	[]field{
-	// 		field{
-	// 			fieldName: "",
-	// 			typeName:  "int",
-	// 		},
-	// 		field{
-	// 			fieldName: "",
-	// 			typeName:  "bool",
-	// 		},
-	// 	},
-	// 	[]ast.Stmt{
-	// 		&ast.ReturnStmt{
-	// 			Results: []ast.Expr{
-	// 				newBasicLit("0"),
-	// 				newBasicLit("false"),
-	// 			},
-	// 		},
-	// 	})
-
-	// printer.Fprint(os.Stdout, fset, f)
 }
