62a7dffbedb7f19936c524b18a91fd9b3edea90f
David Green <dave@activelylazy.co.uk>
2017-04-03 07:51:40 +0100

    Remove dead code

--- cmd/wash/main.go
+++ cmd/wash/main.go
@@ -7,20 +7,6 @@ import (
 )
 
 func main() {
-
-	// basePath := flag.String("base", "", "the base path to read/write source code")
-
-	// flag.Parse()
-
-	// if *basePath == "" {
-	// 	log.Fatalf("Base path is required")
-	// }
-
-	// washer, err := wash.NewWasher(*basePath)
-	// if err != nil {
-	// 	log.Fatalf("Error parsing: %v", err)
-	// }
-
 	wash.SetBasePath("github.com/activelylazy/generated-vending")
 	vendingFile := wash.CreateFile("vending/vending.go", "vending")
 	vendingTestFile := wash.CreateFile("vending/vending_test.go", "vending")
